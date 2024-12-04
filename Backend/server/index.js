094e863b8240e2ed4c670447d37a38f621a64c09
Ankit <sam.megh0305@gmail.com>
2024-12-04 19:29:16 +0530

    work of contact page

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -1,3 +1,4 @@
+const { name } = require('ejs');
 const express = require('express');
 const port = 8080;
 const path = require("path");
@@ -8,6 +9,7 @@ app.set("views", path.join(__dirname, '..', "views"));
 app.use(express.static(path.join(__dirname, '..', "styless")));
 app.use(express.static(path.join(__dirname, '..', "javascript")));
 app.use('/images', express.static(path.join(__dirname, '..', '..', "img")));
+app.use('/Frontend', express.static(path.join(__dirname, '..', '..', "Frontend")));
 
 let projects = [
     {
@@ -31,16 +33,66 @@ let projects = [
         discription: "fist project created by sam."
     },
 ];
+let contact = [
+
+    {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    }, {
+        name: "1st",
+        mobile_no: "1234567890",
+        email: "234sdfg@fghghj.fghv",
+        message: "asdfertyuxcvbnm,sdty"
+    },
+];
+let i = 0;
 
 app.post("/post", (req, res) => {
     let { name, img, discription } = req.body;
-    projects.push({ name, img, discription })
-    res.redirect()
+    projects.push({ name, img, discription });
+    res.redirect('projects.ejs', { projects });
 
 })
 app.get("/projects", (req, res) => {
     res.render('projects.ejs', { projects });
 
+});
+app.post("/contactus", (req, res) => {
+    let { name, mobile_no, email, message } = req.body;
+    contact.push({ name, mobile_no, email, message });
+    res.redirect('contacts/contact.ejs', { contact }, i);
+
+
+});
+app.get("/contacts", (req, res) => {
+    res.render('contact.ejs', { contact, i });
 });
 app.listen(port, () => {
     console.log("server started");
