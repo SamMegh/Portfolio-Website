2e42c7c1cf0fc313ab43e2b7646a147c2b4a3220
Sam <sam.megh0305@gmail.com>
2025-01-30 16:29:46 +0530

    navbar added to each page

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,32 @@ const multer = require("multer");
 const path = require("path");
 const port = 8080;
 
+let filname ;
+
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false })); //for except qurry from get request
+
+// all path to use
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "styless")));
+app.use(express.static(path.join(__dirname, "javascript")));
+app.use("/images", express.static(path.join(__dirname, "..", "images")));
+app.use("/project_images", express.static(path.join(__dirname, "..", "project_images")));
+app.use("/client", express.static(path.join(__dirname, "..", "client")));
+
+
+
 // storage for multer to store image
 const storage = multer.diskStorage({
     destination: function (req, file, cb){
     return cb(null, "./project_images");//in future login condition was written at here if the user is loggin then request further transfer othrewise not
   },
   filename: function (req, file, cb) {
-   return cb(null, `${Date.now()} - ${file.originalname}`);
+    let filname=`${Date.now()}-${file.originalname}`;
+   return cb(null,filname );
   }
 });
 const upload = multer({storage })
@@ -22,8 +41,9 @@ main()
   .catch((err) => {
     console.log(err);
   });
+
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect("mongodb://127.0.0.1:27017/portfolio_website");
 }
 
 // schema for tabe
@@ -42,41 +62,45 @@ const projectschema = new mongoose.Schema({
     required: [true, "name required"],
   },
 });
+
+
+
 // model of mongoos
 const Project=mongoose.model("Project", projectschema);
-
-const app = express();
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: false })); //for except qurry from get request
-
-// all path to use
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "styless")));
-app.use(express.static(path.join(__dirname, "javascript")));
-app.use("/images", express.static(path.join(__dirname, "..", "images")));
-app.use("/Frontend", express.static(path.join(__dirname, "..", "Frontend")));
 
 
 
 // handling main page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "Frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
 
 // handling new Project requests
 app.post("/projects/new", upload.single('img'),(req, res) => {
-  let { name, img, discription } = req.body;
+  let { name, discription } = req.body;
+  img=`/${req.file.path}`;
+  img=img.replace(/\\/g, "/");
   const data = {
     name,
     img,
     discription
   }
- console.log(data);
+
+ Project.collection.insertOne(data).catch(err=>{
+  console.log(err);
+ });
+
+//  res.redirect("/projects")
+ 
 });
 
 app.get("/projects", (req, res) => {
-  res.render("projects.ejs");
+  async function exicute(){
+   let allproject=await Project.collection.find().toArray();
+    await res.render("projects.ejs",{allproject} );
+    
+  }
+  exicute();
 });
 
 // handling contact requests
@@ -84,9 +108,14 @@ app.post("/contactus", (req, res) => {
   let { name, mobile_no, email, message } = req.body;
   contact.push({ name, mobile_no, email, message });
 });
-app.get("/contacts", (req, res) => {
-  res.render("contact.ejs", { contact, i });
+// app.get("/contacts", (req, res) => {
+//   res.render("contact.ejs", { contact, i });
+// });
+
+app.get("/contact",(req,res)=>{
+  res.sendFile(path.join(__dirname, "..", "client", "contact.html"));
 });
+
 app.listen(port, () => {
   console.log("server started");
 });
